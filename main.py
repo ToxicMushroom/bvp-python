c831c1fb916387613a56f48941bdbe8fb990dc49
ToxicMushroom <32853531+ToxicMushroom@users.noreply.github.com>
2021-03-04 17:36:37 +0100

    refactor: pain peko

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from graphics import GraphicsWindow
+
 window = GraphicsWindow(400, 200)
 canvas = window.canvas()
 canvas.setBackground("red")
--- tweedegraadsfunctie.py
+++ tweedegraadsfunctie.py
@@ -6,15 +6,14 @@ a = int(input("geef waarde voor a: "))
 b = int(input("geef waarde voor b: "))
 c = int(input("geef waarde voor c: "))
 
-discriminant = (b**2) - 4*a*c
+discriminant = (b ** 2) - 4 * a * c
 x1 = (-b + discriminant ** 0.5) / (2 * a)
 x2 = (-b - discriminant ** 0.5) / (2 * a)
 
 print(x1, x2)
 
-
 # check
-y1 = a*math.pow(x1, 2) + b*x1 + c
-y2 = a*math.pow(x2, 2) + b*x2 + c
+y1 = a * math.pow(x1, 2) + b * x1 + c
+y2 = a * math.pow(x2, 2) + b * x2 + c
 
 print(y1, y2)
